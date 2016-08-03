fe429b49e2beec98953a4b5dca42d7f8be1a3652
陈子云 <472206618@qq.com>
2016-08-03 16:23:50 +0800

    更新了一些工具函数

--- dist/jqfree.js
+++ dist/jqfree.js
@@ -518,3 +518,79 @@ $.extend({
         }
     }
 });
+
+//units
+$.extend({
+    //$.parseTime(new Date().getTime(), 'YYYY-MM-DD hh:mm:ss')
+    //result: "2016-08-03 16:14:12"
+    parseTime: function (timeStamp, format) {
+        var date = new Date(timeStamp);
+        var o = { 
+            'M+' : date.getMonth() + 1, //month 
+            'D+' : date.getDate(), //day 
+            'h+' : date.getHours(), //hour 
+            'm+' : date.getMinutes(), //minute 
+            's+' : date.getSeconds(), //second 
+            'S' : date.getMilliseconds() //millisecond 
+        } 
+
+        if(/(Y+)/.test(format)) { 
+            format = format.replace(RegExp.$1, 
+                (date.getFullYear() + '').substr(4 - RegExp.$1.length)); 
+        } 
+
+        for(var k in o) { 
+            if (new RegExp('('+ k +')').test(format)) { 
+                format = format.replace(RegExp.$1, 
+                    RegExp.$1.length == 1 ? o[k] : ('00'+ o[k]).substr((''+ o[k]).length)); 
+            } 
+        } 
+        return format; 
+    },
+
+    //$.parseUrl(location.href)
+    //return an object contains the folling info.
+    parseUrl: function (url) {
+        var a =  document.createElement('a');
+        a.href = url;
+        return {
+            source: url,
+            protocol: a.protocol.replace(':',''),
+            host: a.hostname,
+            port: a.port,
+            query: a.search,
+            params: (function(){
+                var ret = {},
+                    seg = a.search.replace(/^\?/,'').split('&'),
+                    len = seg.length, i = 0, s;
+                for (;i<len;i++) {
+                    if (!seg[i]) { continue; }
+                    s = seg[i].split('=');
+                    ret[s[0]] = s[1];
+                }
+                return ret;
+            })(),
+            file: (a.pathname.match(/\/([^\/?#]+)$/i) || [,''])[1],
+            hash: a.hash.replace('#',''),
+            path: a.pathname.replace(/^([^\/])/,'/$1'),
+            relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [,''])[1],
+            segments: a.pathname.replace(/^\//,'').split('/')
+        };
+    },
+
+    //$.toFixedFloat(15.658, 2)
+    //result: 15.66
+    toFixedFloat: function (value, precision) {
+        var power = Math.pow(10, precision || 0);
+        return String(Math.round(value * power) / power);
+    },
+
+    //for generate random string
+    //$.generateRandomAlphaNum(5)
+    //random result: like "rc3sr".
+    generateRandomAlphaNum: function (len) {
+        var rdmString = '';
+        for (; rdmString.length < len; rdmString += Math.random().toString(36).substr(2));
+        return rdmString.substr(0, len);
+    }
+});
